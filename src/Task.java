879475fe7c9889b48efe8e2996c58db2773b9e3d
sReez <step.reznikoff@yandex.ru>
2022-12-14 14:46:39 +0300

    feat: реализовать ТЗ-3

---
+++ src/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    String name;
+    String description;
+    int id;
+    String status;
+
+    public Task(String name, String description, int id, String status) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.status = status;
+    }
+}
